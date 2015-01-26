b4527587a12fa46d0b23ceeeb40741d02d88b54a
Amy Michelle Johnson & Kathryn Carr <student@epicodus.com>
2015-01-26 10:46:00 -0800

    Add webapp functionality for employee class

--- app.rb
+++ app.rb
@@ -4,3 +4,20 @@ also_reload("/lib/**/*.rb")
 require("sinatra/activerecord")
 require("./lib/employee")
 require('pg')
+
+get("/") do
+  @employees = Employee.all()
+  erb(:index)
+end
+
+post("/employee") do
+  @name = params.fetch("name")
+  Employee.create({:name => @name})
+  @employees = Employee.all()
+  erb(:index)
+end
+
+get("/employee/:id") do
+
+
+end
