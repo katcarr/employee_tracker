f275db24f51a217ce3fd090d9ead9e9b198d27f3
Amy Michelle Johnson & Kathryn Carr <student@epicodus.com>
2015-01-26 16:23:35 -0800

    Add ability to see all employees of a division with pull down divisions

--- app.rb
+++ app.rb
@@ -10,12 +10,23 @@ require("pry")
 get("/") do
   @divisions = Division.all()
   @employees = Employee.all()
+  @division_employees = []
   erb(:index)
 end
 
 post("/division") do
   @name = params.fetch("name")
   Division.create({:name => @name})
+  @divisions = Division.all()
+  @employees = Employee.all()
+  @division_employees = []
+  erb(:index)
+end
+
+post("/list_division_employees") do
+  division_id = params.fetch("division_id").to_i()
+  division = Division.find(division_id)
+  @division_employees = division.employees()
   @divisions = Division.all()
   @employees = Employee.all()
   erb(:index)
@@ -30,5 +41,6 @@ post("/division/added_employee") do
   @found_division.save()
   @employees = Employee.all()
   @divisions = Division.all()
+  @division_employees = []
   erb(:index)
 end
